4be00b0f7ce111dd1ffb9cd9988277e0715c13d9
arthur.valente.reis <arthur.valente.reis@gmail.com>
2024-06-25 22:17:43 -0300

    :sparkles: Creating task endpoints

---
+++ TaskLand.API/Controllers/Base/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskLand.API.Controllers.Base
+{
+    public class BaseController : ControllerBase
+    {
+        protected string IpAddress()
+        {
+            // get source ip address for the current request
+            if (Request.Headers.ContainsKey("X-Forwarded-For"))
+                return Request.Headers["X-Forwarded-For"];
+            else
+                return HttpContext?.Connection?.RemoteIpAddress?.MapToIPv4().ToString();
+        }
+    }
+}
---
+++ TaskLand.API/Controllers/TaskController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using TaskLand.API.Controllers.Base;
+using TaskLand.API.DTO.Request;
+using TaskLand.API.DTO.Response;
+using TaskLand.API.Interfaces.Services;
+using TaskLand.API.Model;
+
+namespace TaskLand.API.Controllers
+{
+    public class TaskController : BaseController
+    {
+        private ITaskService _taskService;
+        public TaskController(
+            ITaskService taskService) : base()
+
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet("task/{id}")]
+        [ProducesResponseType(typeof(ResponseTask), 200)]
+        [SwaggerOperation(
+            Summary = "Busca uma task através do ID"
+        )]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(long id)
+        {
+            try
+            {
+                var task = await _taskService.GetTask(id);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("task")]
+        [ProducesResponseType(typeof(List<ResponseTask>), StatusCodes.Status200OK)]
+        [SwaggerOperation(
+            Summary = "Busca uma lista de task"
+        )]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> List()
+        {
+            try
+            {
+                var task = await _taskService.List();
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(List<ResponseTask>), StatusCodes.Status201Created)]
+        [SwaggerOperation(
+            Summary = "Busca uma lista de task"
+        )]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Create([FromBody] RequestTaskCreate taskRequest)
+        {
+            try
+            {
+                var taskCreated = await _taskService.Create(taskRequest);
+                return Ok(taskCreated);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("update")]
+        [ProducesResponseType(typeof(ResponseTask), StatusCodes.Status200OK)]
+        [SwaggerOperation(
+            Summary = "Atualiza uma task"
+        )]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update([FromBody] RequestTaskCreate taskRequest )
+        {
+            try
+            {
+                var taskCreated = await _taskService.Create(taskRequest);
+                return Ok(taskCreated);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    
+        [HttpDelete("delete/{id}")]
+        [ProducesResponseType(typeof(ResponseTask), StatusCodes.Status200OK)]
+        [SwaggerOperation(
+            Summary = "Deleta uma task"
+        )]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update(long id)
+        {
+            try
+            {
+                var taskCreated = await _taskService.Delete(id);
+                return Ok(taskCreated);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
